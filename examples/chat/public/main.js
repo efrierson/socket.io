c4ca6041e1ba7fb22d1542cd20fa6c650591615c
Eric Frierson <efrierson@ebscohost.com>
2015-10-31 15:02:47 -0500

    no message

--- examples/chat/public/main.js
+++ examples/chat/public/main.js
@@ -89,7 +89,7 @@ $(function() {
       .css('color', getUsernameColor(data.username));
     //var $messageBodyDiv = $('<span class="messageBody">')
     //  .text(data.message);
-    var $messageBodyDiv = $('<iframe width="300" height="380" frameborder="0" allowtransparency="true">').attr("src","https://embed.spotify.com/?uri="+data.message);
+    var $messageBodyDiv = $('<iframe width="300" height="380" frameborder="0" allowtransparency="true">').attr("src","https://play.spotify.com/track/"+data.message+"?play=true");
     
     var typingClass = data.typing ? 'typing' : '';
     var $messageDiv = $('<li class="message"/>')
